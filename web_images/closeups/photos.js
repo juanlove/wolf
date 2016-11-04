a303482bde17ebe2c99e6950bad0e476154d59ac
Juan Urena <Juan Urena>
2016-11-03 20:37:35 -0700

    first commit

---
+++ web_images/closeups/photos.js
@@ -0,0 +1,344 @@
+WPGgallery = new Object();
+WPGgallery.name = "Tokuyama%20Product%20Photos";
+WPGgallery.photographer = "SEM%20Filler%20Sigma";
+WPGgallery.contact = "a%20Site%20Foresight%20Project";
+WPGgallery.email = "";
+WPGgallery.date = "5/23/2006"
+
+WPGgallery.colors = new Object();
+WPGgallery.colors.background = "#FFFFFF";
+WPGgallery.colors.banner = "#F0F0F0";
+WPGgallery.colors.text = "#000000";
+WPGgallery.colors.link = "#0000FF";
+WPGgallery.colors.alink = "#FF0000";
+WPGgallery.colors.vlink = "#800080";
+
+gPhotos = new Array();
+gPhotos[0] = new Object();
+gPhotos[0].filename = "33StreetPlatform_GS.jpg";
+gPhotos[0].ImageWidth = 850;
+gPhotos[0].ImageHeight = 1025;
+gPhotos[0].ThumbWidth = 116;
+gPhotos[0].ThumbHeight = 140;
+gPhotos[0].meta = new Object();
+
+gPhotos[1] = new Object();
+gPhotos[1].filename = "BlvdDrinks%232_fullview_GS.jpg";
+gPhotos[1].ImageWidth = 897;
+gPhotos[1].ImageHeight = 1025;
+gPhotos[1].ThumbWidth = 122;
+gPhotos[1].ThumbHeight = 140;
+gPhotos[1].meta = new Object();
+
+gPhotos[2] = new Object();
+gPhotos[2].filename = "BlvdDrinks(%231)_GS.jpg";
+gPhotos[2].ImageWidth = 1025;
+gPhotos[2].ImageHeight = 1050;
+gPhotos[2].ThumbWidth = 136;
+gPhotos[2].ThumbHeight = 140;
+gPhotos[2].meta = new Object();
+
+gPhotos[3] = new Object();
+gPhotos[3].filename = "Canal%26Broadway_GS.jpg";
+gPhotos[3].ImageWidth = 1025;
+gPhotos[3].ImageHeight = 437;
+gPhotos[3].ThumbWidth = 140;
+gPhotos[3].ThumbHeight = 59;
+gPhotos[3].meta = new Object();
+
+gPhotos[4] = new Object();
+gPhotos[4].filename = "FlamingoMotorCourt_GS.jpg";
+gPhotos[4].ImageWidth = 1024;
+gPhotos[4].ImageHeight = 899;
+gPhotos[4].ThumbWidth = 140;
+gPhotos[4].ThumbHeight = 122;
+gPhotos[4].meta = new Object();
+
+gPhotos[5] = new Object();
+gPhotos[5].filename = "FolliesBurlesk_front_GS.jpg";
+gPhotos[5].ImageWidth = 1025;
+gPhotos[5].ImageHeight = 683;
+gPhotos[5].ThumbWidth = 140;
+gPhotos[5].ThumbHeight = 93;
+gPhotos[5].meta = new Object();
+
+gPhotos[6] = new Object();
+gPhotos[6].filename = "Gasland_interior_GS.jpg";
+gPhotos[6].ImageWidth = 1025;
+gPhotos[6].ImageHeight = 670;
+gPhotos[6].ThumbWidth = 140;
+gPhotos[6].ThumbHeight = 91;
+gPhotos[6].meta = new Object();
+
+gPhotos[7] = new Object();
+gPhotos[7].filename = "HotelCavalier_GS.jpg";
+gPhotos[7].ImageWidth = 1025;
+gPhotos[7].ImageHeight = 691;
+gPhotos[7].ThumbWidth = 140;
+gPhotos[7].ThumbHeight = 94;
+gPhotos[7].meta = new Object();
+
+gPhotos[8] = new Object();
+gPhotos[8].filename = "HotelHallway_GS.jpg";
+gPhotos[8].ImageWidth = 1025;
+gPhotos[8].ImageHeight = 991;
+gPhotos[8].ThumbWidth = 140;
+gPhotos[8].ThumbHeight = 135;
+gPhotos[8].meta = new Object();
+
+gPhotos[9] = new Object();
+gPhotos[9].filename = "HudsonHotel_GS.jpg";
+gPhotos[9].ImageWidth = 1025;
+gPhotos[9].ImageHeight = 741;
+gPhotos[9].ThumbWidth = 140;
+gPhotos[9].ThumbHeight = 101;
+gPhotos[9].meta = new Object();
+
+gPhotos[10] = new Object();
+gPhotos[10].filename = "LorAl_GS.jpg";
+gPhotos[10].ImageWidth = 1025;
+gPhotos[10].ImageHeight = 754;
+gPhotos[10].ThumbWidth = 140;
+gPhotos[10].ThumbHeight = 102;
+gPhotos[10].meta = new Object();
+
+gPhotos[11] = new Object();
+gPhotos[11].filename = "LuckySevens_side_GS.jpg";
+gPhotos[11].ImageWidth = 1025;
+gPhotos[11].ImageHeight = 662;
+gPhotos[11].ThumbWidth = 140;
+gPhotos[11].ThumbHeight = 90;
+gPhotos[11].meta = new Object();
+
+gPhotos[12] = new Object();
+gPhotos[12].filename = "Ma%27s_GS.jpg";
+gPhotos[12].ImageWidth = 1025;
+gPhotos[12].ImageHeight = 783;
+gPhotos[12].ThumbWidth = 140;
+gPhotos[12].ThumbHeight = 106;
+gPhotos[12].meta = new Object();
+
+gPhotos[13] = new Object();
+gPhotos[13].filename = "MidtownDiner_GS.jpg";
+gPhotos[13].ImageWidth = 1025;
+gPhotos[13].ImageHeight = 693;
+gPhotos[13].ThumbWidth = 140;
+gPhotos[13].ThumbHeight = 94;
+gPhotos[13].meta = new Object();
+
+gPhotos[14] = new Object();
+gPhotos[14].filename = "MissAmerica_fullview_GS.jpg";
+gPhotos[14].ImageWidth = 1025;
+gPhotos[14].ImageHeight = 770;
+gPhotos[14].ThumbWidth = 140;
+gPhotos[14].ThumbHeight = 105;
+gPhotos[14].meta = new Object();
+
+gPhotos[15] = new Object();
+gPhotos[15].filename = "Moe%27sLuncheonette_GS.jpg";
+gPhotos[15].ImageWidth = 1025;
+gPhotos[15].ImageHeight = 500;
+gPhotos[15].ThumbWidth = 140;
+gPhotos[15].ThumbHeight = 68;
+gPhotos[15].meta = new Object();
+
+gPhotos[16] = new Object();
+gPhotos[16].filename = "MrLucky_GS.jpg";
+gPhotos[16].ImageWidth = 1025;
+gPhotos[16].ImageHeight = 512;
+gPhotos[16].ThumbWidth = 140;
+gPhotos[16].ThumbHeight = 69;
+gPhotos[16].meta = new Object();
+
+gPhotos[17] = new Object();
+gPhotos[17].filename = "NY%20Army-Navy_GS.jpg";
+gPhotos[17].ImageWidth = 1025;
+gPhotos[17].ImageHeight = 816;
+gPhotos[17].ThumbWidth = 140;
+gPhotos[17].ThumbHeight = 111;
+gPhotos[17].meta = new Object();
+
+gPhotos[18] = new Object();
+gPhotos[18].filename = "NYBEST_GS.jpg";
+gPhotos[18].ImageWidth = 1025;
+gPhotos[18].ImageHeight = 964;
+gPhotos[18].ThumbWidth = 140;
+gPhotos[18].ThumbHeight = 131;
+gPhotos[18].meta = new Object();
+
+gPhotos[19] = new Object();
+gPhotos[19].filename = "Nathans.jpg";
+gPhotos[19].ImageWidth = 1025;
+gPhotos[19].ImageHeight = 738;
+gPhotos[19].ThumbWidth = 140;
+gPhotos[19].ThumbHeight = 100;
+gPhotos[19].meta = new Object();
+
+gPhotos[20] = new Object();
+gPhotos[20].filename = "Non-stopAction_GS.jpg";
+gPhotos[20].ImageWidth = 1024;
+gPhotos[20].ImageHeight = 840;
+gPhotos[20].ThumbWidth = 140;
+gPhotos[20].ThumbHeight = 114;
+gPhotos[20].meta = new Object();
+
+gPhotos[21] = new Object();
+gPhotos[21].filename = "OccupiedHotelRoom1_800.jpg";
+gPhotos[21].ImageWidth = 1025;
+gPhotos[21].ImageHeight = 666;
+gPhotos[21].ThumbWidth = 140;
+gPhotos[21].ThumbHeight = 90;
+gPhotos[21].meta = new Object();
+
+gPhotos[22] = new Object();
+gPhotos[22].filename = "ParadiseMotel_GS.jpg";
+gPhotos[22].ImageWidth = 1025;
+gPhotos[22].ImageHeight = 712;
+gPhotos[22].ThumbWidth = 140;
+gPhotos[22].ThumbHeight = 97;
+gPhotos[22].meta = new Object();
+
+gPhotos[23] = new Object();
+gPhotos[23].filename = "PennAveElevated_GS.jpg";
+gPhotos[23].ImageWidth = 1025;
+gPhotos[23].ImageHeight = 662;
+gPhotos[23].ThumbWidth = 140;
+gPhotos[23].ThumbHeight = 90;
+gPhotos[23].meta = new Object();
+
+gPhotos[24] = new Object();
+gPhotos[24].filename = "PhoneBooth.jpg";
+gPhotos[24].ImageWidth = 895;
+gPhotos[24].ImageHeight = 1025;
+gPhotos[24].ThumbWidth = 122;
+gPhotos[24].ThumbHeight = 140;
+gPhotos[24].meta = new Object();
+
+gPhotos[25] = new Object();
+gPhotos[25].filename = "PlayboyBilliards(ext)_GS.jpg";
+gPhotos[25].ImageWidth = 1025;
+gPhotos[25].ImageHeight = 825;
+gPhotos[25].ThumbWidth = 140;
+gPhotos[25].ThumbHeight = 112;
+gPhotos[25].meta = new Object();
+
+gPhotos[26] = new Object();
+gPhotos[26].filename = "ROOFTOP_overall_GS.jpg";
+gPhotos[26].ImageWidth = 1025;
+gPhotos[26].ImageHeight = 837;
+gPhotos[26].ThumbWidth = 140;
+gPhotos[26].ThumbHeight = 114;
+gPhotos[26].meta = new Object();
+
+gPhotos[27] = new Object();
+gPhotos[27].filename = "RialtoCinema_GS.jpg";
+gPhotos[27].ImageWidth = 1025;
+gPhotos[27].ImageHeight = 704;
+gPhotos[27].ThumbWidth = 140;
+gPhotos[27].ThumbHeight = 96;
+gPhotos[27].meta = new Object();
+
+gPhotos[28] = new Object();
+gPhotos[28].filename = "St.GeoHotel_GS.jpg";
+gPhotos[28].ImageWidth = 1025;
+gPhotos[28].ImageHeight = 775;
+gPhotos[28].ThumbWidth = 140;
+gPhotos[28].ThumbHeight = 105;
+gPhotos[28].meta = new Object();
+
+gPhotos[29] = new Object();
+gPhotos[29].filename = "StreetCorner(%231)_overall_GS.jpg";
+gPhotos[29].ImageWidth = 695;
+gPhotos[29].ImageHeight = 506;
+gPhotos[29].ThumbWidth = 140;
+gPhotos[29].ThumbHeight = 101;
+gPhotos[29].meta = new Object();
+
+gPhotos[30] = new Object();
+gPhotos[30].filename = "StreetCorner(%232)_GS.jpg";
+gPhotos[30].ImageWidth = 1024;
+gPhotos[30].ImageHeight = 691;
+gPhotos[30].ThumbWidth = 140;
+gPhotos[30].ThumbHeight = 94;
+gPhotos[30].meta = new Object();
+
+gPhotos[31] = new Object();
+gPhotos[31].filename = "SubwayEntrance_GS.jpg";
+gPhotos[31].ImageWidth = 1025;
+gPhotos[31].ImageHeight = 825;
+gPhotos[31].ThumbWidth = 140;
+gPhotos[31].ThumbHeight = 112;
+gPhotos[31].meta = new Object();
+
+gPhotos[32] = new Object();
+gPhotos[32].filename = "SubwayKiosk_GS.jpg";
+gPhotos[32].ImageWidth = 943;
+gPhotos[32].ImageHeight = 666;
+gPhotos[32].ThumbWidth = 140;
+gPhotos[32].ThumbHeight = 98;
+gPhotos[32].meta = new Object();
+
+gPhotos[33] = new Object();
+gPhotos[33].filename = "Terminal%20Diner_GS.jpg";
+gPhotos[33].ImageWidth = 1022;
+gPhotos[33].ImageHeight = 777;
+gPhotos[33].ThumbWidth = 140;
+gPhotos[33].ThumbHeight = 106;
+gPhotos[33].meta = new Object();
+
+gPhotos[34] = new Object();
+gPhotos[34].filename = "TimesSq.HotelRoom_GS.jpg";
+gPhotos[34].ImageWidth = 1025;
+gPhotos[34].ImageHeight = 737;
+gPhotos[34].ThumbWidth = 140;
+gPhotos[34].ThumbHeight = 100;
+gPhotos[34].meta = new Object();
+
+gPhotos[35] = new Object();
+gPhotos[35].filename = "ToAtlanticAve_GS.jpg";
+gPhotos[35].ImageWidth = 1025;
+gPhotos[35].ImageHeight = 700;
+gPhotos[35].ThumbWidth = 140;
+gPhotos[35].ThumbHeight = 95;
+gPhotos[35].meta = new Object();
+
+gPhotos[36] = new Object();
+gPhotos[36].filename = "TrueLoveInBrooklyn_GS.jpg";
+gPhotos[36].ImageWidth = 1025;
+gPhotos[36].ImageHeight = 391;
+gPhotos[36].ThumbWidth = 140;
+gPhotos[36].ThumbHeight = 53;
+gPhotos[36].meta = new Object();
+
+gPhotos[37] = new Object();
+gPhotos[37].filename = "TubeBar_GS.jpg";
+gPhotos[37].ImageWidth = 1025;
+gPhotos[37].ImageHeight = 783;
+gPhotos[37].ThumbWidth = 140;
+gPhotos[37].ThumbHeight = 106;
+gPhotos[37].meta = new Object();
+
+gPhotos[38] = new Object();
+gPhotos[38].filename = "VillageCigars_GS.jpg";
+gPhotos[38].ImageWidth = 1025;
+gPhotos[38].ImageHeight = 695;
+gPhotos[38].ThumbWidth = 140;
+gPhotos[38].ThumbHeight = 94;
+gPhotos[38].meta = new Object();
+
+gPhotos[39] = new Object();
+gPhotos[39].filename = "WhiteTower_GS.jpg";
+gPhotos[39].ImageWidth = 1025;
+gPhotos[39].ImageHeight = 708;
+gPhotos[39].ThumbWidth = 140;
+gPhotos[39].ThumbHeight = 96;
+gPhotos[39].meta = new Object();
+
+gPhotos[40] = new Object();
+gPhotos[40].filename = "WildGirls_overall_GS.jpg";
+gPhotos[40].ImageWidth = 1024;
+gPhotos[40].ImageHeight = 854;
+gPhotos[40].ThumbWidth = 140;
+gPhotos[40].ThumbHeight = 116;
+gPhotos[40].meta = new Object();
+
